f66e6198477fa68a2c059b4a5febde5a97bfb7dc
ktmllr <katharinemillernz@gmail.com>
2019-05-30 21:42:44 +1200

    5

---
+++ Kitten_Mittens/shop.js
@@ -0,0 +1,99 @@
+
+
+    function doMouseOver()
+    {
+
+        document.getElementById("mitten1").src = "images/socks11.jpg";
+
+    }
+        
+    function doMouseOut()
+    {
+
+        document.getElementById("mitten1").src = "images/socks1.png";
+
+    }
+
+       
+
+
+function doMouseOver()
+    {
+
+        document.getElementById("mitten2").src = "images/socks22.png";
+
+    }
+        
+    function doMouseOut()
+    {
+
+        document.getElementById("mitten2").src = "images/socks2.png";
+
+    }
+    
+
+        
+        function doMouseOver()
+    {
+
+        document.getElementById("mitten3").src = "images/socks33.png";
+
+    }
+        
+    function doMouseOut()
+    {
+
+        document.getElementById("mitten3").src = "images/socks3.png";
+
+    }
+      
+
+        
+        function doMouseOver()
+    {
+
+        document.getElementById("mitten4").src = "images/socks44.png";
+
+    }
+        
+    function doMouseOut()
+    {
+
+        document.getElementById("mitten4").src = "images/socks4.png";
+
+    }
+       
+
+        
+        
+        
+        function doMouseOver()
+    {
+
+        document.getElementById("mitten5").src = "images/socks55.png";
+
+    }
+        
+    function doMouseOut()
+    {
+
+        document.getElementById("mitten5").src = "images/socks5.png";
+
+    }
+
+        
+        
+        
+        function doMouseOver()
+    {
+
+        document.getElementById("mitten6").src = "images/socks66.png";
+
+    }
+        
+    function doMouseOut()
+    {
+
+        document.getElementById("mitten6").src = "images/socks6.png";
+
+    }
